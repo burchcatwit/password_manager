348b9698d4bff8897b9d582556f80ace8301eb29
Andrew Hogan <hogana3@wit.edu>
2022-04-13 17:50:26 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
+import Home from '../src/Components/home/home';
+import Portal from './appholder.js';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import logo from '../src/Components/images/mainlogo.png';
+import mockup from '../src/Components/images/MOCKUPS.png';
+import LogIN from  '../src/Components/forms/loginFORM';
+import CreateForm from '../src/Components/forms/createaccountForm'; 
+import { useState } from 'react';
+import { PasswordGenerator, PasswordList } from "./appholder.js";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Router>
+	<Switch>
+	<Route exact path='/' component={Home}/>
+	 </Switch>
+	<Switch>  
+        <Route exact path='/Dashboard' component={Portal}/>
+	</Switch>
+	<Switch>
+	<Route exact path='/password-list' element={<PasswordList/>} />
+	</Switch>
+	<Switch>
+	<Route exact path='/password-generator' element={<PasswordGenerator/>} />
+	</Switch>
+    </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
