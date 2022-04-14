8ef898bfde6ff58a6b492e1059782de98dbb06f2
Andrew Hogan <hogana3@wit.edu>
2022-04-14 00:00:15 -0400

    Testing icon

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import './appholder.css';
+import './font.css';
 import Home from '../src/Components/home/home';
 import App from './appholder';
 import { NavHeader } from './appholder.js';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import logo from '../src/Components/images/mainlogo.png';
-import mockup from '../src/Components/images/MOCKUPS.png';
-import LogIN from  '../src/Components/forms/loginFORM';
-import CreateForm from '../src/Components/forms/createaccountForm'; 
-import { useState } from 'react';
 import { PasswordGenerator, PasswordList } from "./appholder.js";
 
 
